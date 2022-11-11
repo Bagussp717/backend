a0b526d4ad888e21646a24a3219f7085d2c85712
bagus <91867610+Bagussp717@users.noreply.github.com>
2022-11-11 20:17:59 +0700

    Update render.js

--- render.js
+++ render.js
@@ -23,23 +23,26 @@ var server = http.createServer(function(request,response){
     if (q.pathname == "/" && request.method == "GET"){
         var keyword = q.query.keyword;
         if (keyword){
-            fs.readFile(renderHasil,(error,data)=>{
-                if (error){
-                    response.writeHead(404,{"Content-Type":"text/html"});
-                    return response.end("404 Server Not Found");                
-                }
-                else{
-                    response.writeHead(200, {"Content-Type":"text/html"});
-                    response.write(data)
-                    response.write("<h2>Pencarian</h2>");
-                    response.write("<p>Anda Mencari : <b>" + keyword + "</b> </p>");
-                    response.write("<h3><b></b>Tidak ada Hasil ! Maaf Website ini masih dalam tahap pengembangan</b></h3>");
-                    response.end("<a class='back' href='/'>Kembali</a>");
-                    return response.end()
-                }
+            response.writeHead(200, {"Content-Type":"text/html"});
+            response.write(data)
+            response.write("<h2>Pencarian</h2>");
+            response.write("<p>Anda Mencari : <b>" + keyword + "</b> </p>");
+            response.write("<h3><b></b>Tidak ada Hasil ! Maaf Website ini masih dalam tahap pengembangan</b></h3>");
+            response.end("<a class='back' href='/'>Kembali</a>");
+            }
+        else{
+            fs.readFile("home.html",function(error,data){
+            if (error){
+                response.writeHead(404,{"Content-Type":"text/html"});
+                return response.end("404 Server Not Found"); 
+            }
+            response.writeHead(200, {"Content-Type":"text/html"});
+            response.write(data)
+            response.end()
             });
         }
     }
+
     else if (request.url==="/login" && request.method === "GET"){
         fs.readFile("login.html",(error,data)=>{
             if (error){
